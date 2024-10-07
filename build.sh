be9227f4156e0a8ea7582a3ff46941026f1b098a
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-10-07 01:26:39 +0100

    WOO

--- build.sh
+++ build.sh
@@ -6,6 +6,13 @@ sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install -y git zip unzip openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses-dev cmake libffi-dev libssl-dev libtinfo-dev libbz2-dev libdb5.3-dev libgdbm-dev liblzma-dev tk-dev uuid-dev libreadline-dev libsqlite3-dev lld
 
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+buildozer android release
+
 end_time=$(date +%s)
 
 duration=$((end_time - start_time))
